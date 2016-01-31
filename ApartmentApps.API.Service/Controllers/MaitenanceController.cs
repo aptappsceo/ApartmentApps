b6776cbe0848aa4fab5d08d5cfab929066ea7180
Micah Osborne <micahosborne@gmail.com>
2016-01-31 11:14:51 -0500

    Small stuff nothing of use yet.

---
+++ ApartmentApps.API.Service/Controllers/MaitenanceController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ApartmentApps.API.Service.Models;
+
+namespace ApartmentApps.API.Service.Controllers
+{
+    [Authorize]
+    public class MaitenanceController : ApiController
+    {
+        public ApplicationDbContext Context { get; set; }
+
+        public MaitenanceController(ApplicationDbContext context)
+        {
+            Context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<MaitenanceRequest> GetByWorker(string workerId)
+        {
+            return Context.MaitenanceRequests.Where(p => p.WorkerId == workerId);
+        }
+    }
+
+
+}
---
+++ ApartmentApps.API.Service/Models/VMS/MaitenanceRequestViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApartmentApps.API.Service.Models.VMS
+{
+    public class MaitenanceRequestFormModel
+    {
+    }
+}
