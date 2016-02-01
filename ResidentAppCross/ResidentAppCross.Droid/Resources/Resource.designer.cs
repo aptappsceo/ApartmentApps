725ac6b16c3ef1e185e2a3b5f91a15b6c0cd72eb
nitreo <sinitreo@gmail.com>
2016-02-01 18:06:18 +0500

    - fix api not compiling due to Unity Container refernce - badges added

--- ResidentAppCross/ResidentAppCross.Droid/Resources/Resource.designer.cs
+++ ResidentAppCross/ResidentAppCross.Droid/Resources/Resource.designer.cs
@@ -139,58 +139,64 @@ namespace ResidentAppCross.Droid
 			public const int background = 2130837510;
 			
 			// aapt resource value: 0x7f020007
-			public const int Border1 = 2130837511;
+			public const int BadgeBackground = 2130837511;
 			
 			// aapt resource value: 0x7f020008
-			public const int HouseIcon = 2130837512;
+			public const int BadgeBackgroundSource = 2130837512;
 			
 			// aapt resource value: 0x7f020009
-			public const int ic_login_50893_0013_forma = 2130837513;
+			public const int Border1 = 2130837513;
 			
 			// aapt resource value: 0x7f02000a
-			public const int Icon = 2130837514;
+			public const int HouseIcon = 2130837514;
 			
 			// aapt resource value: 0x7f02000b
-			public const int icon1 = 2130837515;
+			public const int ic_login_50893_0013_forma = 2130837515;
 			
 			// aapt resource value: 0x7f02000c
-			public const int iconelse = 2130837516;
+			public const int Icon = 2130837516;
 			
 			// aapt resource value: 0x7f02000d
-			public const int LOGIN = 2130837517;
+			public const int icon1 = 2130837517;
 			
 			// aapt resource value: 0x7f02000e
-			public const int LoginButton = 2130837518;
+			public const int iconelse = 2130837518;
 			
 			// aapt resource value: 0x7f02000f
-			public const int logo_03 = 2130837519;
+			public const int LOGIN = 2130837519;
 			
 			// aapt resource value: 0x7f020010
-			public const int MaintenaceIcon = 2130837520;
+			public const int LoginButton = 2130837520;
 			
 			// aapt resource value: 0x7f020011
-			public const int monoandroidsplash = 2130837521;
+			public const int logo_03 = 2130837521;
 			
 			// aapt resource value: 0x7f020012
-			public const int OfficerIcon = 2130837522;
+			public const int MaintenaceIcon = 2130837522;
 			
 			// aapt resource value: 0x7f020013
-			public const int PartnersIcon = 2130837523;
+			public const int monoandroidsplash = 2130837523;
 			
 			// aapt resource value: 0x7f020014
-			public const int PayIcon = 2130837524;
+			public const int OfficerIcon = 2130837524;
 			
 			// aapt resource value: 0x7f020015
-			public const int SecurityIcon = 2130837525;
+			public const int PartnersIcon = 2130837525;
 			
 			// aapt resource value: 0x7f020016
-			public const int SettingsIcon = 2130837526;
+			public const int PayIcon = 2130837526;
 			
 			// aapt resource value: 0x7f020017
-			public const int SignOutIcon = 2130837527;
+			public const int SecurityIcon = 2130837527;
 			
 			// aapt resource value: 0x7f020018
-			public const int UsernameIcon = 2130837528;
+			public const int SettingsIcon = 2130837528;
+			
+			// aapt resource value: 0x7f020019
+			public const int SignOutIcon = 2130837529;
+			
+			// aapt resource value: 0x7f02001a
+			public const int UsernameIcon = 2130837530;
 			
 			static Drawable()
 			{
@@ -394,8 +400,11 @@ namespace ResidentAppCross.Droid
 		public partial class Style
 		{
 			
+			// aapt resource value: 0x7f060002
+			public const int Mono_Android_Theme_Splash = 2131099650;
+			
 			// aapt resource value: 0x7f060001
-			public const int Mono_Android_Theme_Splash = 2131099649;
+			public const int badge_danger = 2131099649;
 			
 			// aapt resource value: 0x7f060000
 			public const int button_text = 2131099648;
--- ResidentAppCross/ResidentAppCross/ApartmentApps.API.Service/Values.cs
+++ ResidentAppCross/ResidentAppCross/ApartmentApps.API.Service/Values.cs
@@ -11,6 +11,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using ApartmentApps.Client;
+using ApartmentApps.Client.Models;
 using Microsoft.Rest;
 using Newtonsoft.Json.Linq;
 
