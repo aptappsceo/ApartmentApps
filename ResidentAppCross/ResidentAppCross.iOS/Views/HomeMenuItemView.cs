73be7d29107b97597d13d73883d7506ef82b81d7
sinitreo - mac <sinitreo@gmail.com>
2016-02-21 08:14:14 -0800

    +1

---
+++ ResidentAppCross/ResidentAppCross.iOS/Views/HomeMenuItemView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Foundation;
+using UIKit;
+
+namespace ResidentAppCross.iOS
+{
+	public partial class HomeMenuItemView : UITableViewCell
+	{
+		public static readonly NSString Key = new NSString ("HomeMenuItemView");
+		public static readonly UINib Nib;
+
+		static HomeMenuItemView ()
+		{
+			Nib = UINib.FromName ("HomeMenuItemView", NSBundle.MainBundle);
+		}
+
+		public HomeMenuItemView (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
