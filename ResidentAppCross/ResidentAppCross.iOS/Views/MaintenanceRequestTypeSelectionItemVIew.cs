d7c5281876461932f64fadc57d8887d046450aac
sinitreo - mac <sinitreo@gmail.com>
2016-02-21 08:13:59 -0800

    screens  files (Design)

---
+++ ResidentAppCross/ResidentAppCross.iOS/Views/MaintenanceRequestTypeSelectionItemVIew.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Foundation;
+using UIKit;
+
+namespace ResidentAppCross.iOS
+{
+	public partial class MaintenanceRequestTypeSelectionItemVIew : UITableViewCell
+	{
+		public static readonly NSString Key = new NSString ("MaintenanceRequestTypeSelectionItemVIew");
+		public static readonly UINib Nib;
+
+		static MaintenanceRequestTypeSelectionItemVIew ()
+		{
+			Nib = UINib.FromName ("MaintenanceRequestTypeSelectionItemVIew", NSBundle.MainBundle);
+		}
+
+		public MaintenanceRequestTypeSelectionItemVIew (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
