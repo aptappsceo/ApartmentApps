27328e09c3bac24a8bb042c5faeec94213ab539d
sinitreo - mac <sinitreo@gmail.com>
2016-02-22 21:29:23 -0800

    - Photo grid outlet added

--- ResidentAppCross/ResidentAppCross.iOS/Views/MaintenanceRequestView.designer.cs
+++ ResidentAppCross/ResidentAppCross.iOS/Views/MaintenanceRequestView.designer.cs
@@ -1,11 +1,13 @@
 // WARNING
 //
-// This file has been generated automatically by Xamarin Studio to store outlets and
-// actions made in the UI designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
 using System.CodeDom.Compiler;
+using UIKit;
 
 namespace ResidentAppCross.iOS
 {
@@ -29,37 +31,32 @@ namespace ResidentAppCross.iOS
 
 		[Outlet]
 		UIKit.UIButton RequestTypeSelectionButton { get; set; }
-		
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UICollectionView PhotoGrid { get; set; }
+
 		void ReleaseDesignerOutlets ()
 		{
 			if (AddPhotoButton != null) {
 				AddPhotoButton.Dispose ();
 				AddPhotoButton = null;
 			}
-
-			if (CommentsTextField != null) {
-				CommentsTextField.Dispose ();
-				CommentsTextField = null;
+			if (CommentsTextView != null) {
+				CommentsTextView.Dispose ();
+				CommentsTextView = null;
 			}
-
 			if (PetTypeSelector != null) {
 				PetTypeSelector.Dispose ();
 				PetTypeSelector = null;
 			}
-
+			if (PhotoGrid != null) {
+				PhotoGrid.Dispose ();
+				PhotoGrid = null;
+			}
 			if (RequestTypeSelectionButton != null) {
 				RequestTypeSelectionButton.Dispose ();
 				RequestTypeSelectionButton = null;
-			}
-
-			if (PhotosContainer != null) {
-				PhotosContainer.Dispose ();
-				PhotosContainer = null;
-			}
-
-			if (CommentsTextView != null) {
-				CommentsTextView.Dispose ();
-				CommentsTextView = null;
 			}
 		}
 	}
