2fbdec504cccf646a116501b7da3028eaa5464b2
sinitreo - mac <sinitreo@gmail.com>
2016-02-26 07:14:09 -0800

    - I hate xamarin studio

--- ResidentAppCross/ResidentAppCross.iOS/Views/MaintenanceRequestFormView.designer.cs
+++ ResidentAppCross/ResidentAppCross.iOS/Views/MaintenanceRequestFormView.designer.cs
@@ -7,7 +7,7 @@
 using Foundation;
 using System.CodeDom.Compiler;
 
-namespace ResidentAppCross.iOS
+namespace ResidentAppCross.iOS.Views
 {
 	[Register ("MaintenanceRequestFormView")]
 	partial class MaintenanceRequestFormView
