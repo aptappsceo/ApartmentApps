d260c3d4682d84c76b7d0b8be49b718c4ed33fea
Micah Osborne <micahosborne@gmail.com>
2016-03-18 13:29:31 -0400

    no message

---
+++ ApartmentApps.API.Service/Controllers/Api/ConfigureController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ApartmentApps.Data;
+
+namespace ApartmentApps.API.Service.Controllers.Api
+{
+    public class ConfigureController : ApartmentAppsApiController
+    {
+        public void AddCourtesyLocation(Guid guid, decimal latitude, decimal longitude)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                if (CurrentUser.PropertyId != null)
+                    ctx.CourtesyOfficerLocations.Add(new CourtesyOfficerLocation()
+                    {
+                        PropertyId = CurrentUser.PropertyId.Value,
+                        Latitude = latitude,
+                        Longitude = longitude,
+                        LocationId = guid.ToString()
+                    });
+            }
+        }
+    }
+}
