71ebf50e637ad1f3b4e74a512f552cb85da6b8e6
sinitreo - mac <sinitreo@gmail.com>
2016-03-20 23:06:52 -0700

    Table Section

---
+++ ResidentAppCross/ResidentAppCross.iOS/Views/Sections/TableSection.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace ResidentAppCross.iOS
+{
+	partial class TableSection : UIView
+	{
+		public TableSection (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
