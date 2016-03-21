c05eca0515d8f178f7c07c68f39311cef5aeb4d3
sinitreo - mac <sinitreo@gmail.com>
2016-03-21 03:42:17 -0700

    - new table ticket view

---
+++ ResidentAppCross/ResidentAppCross.iOS/Views/TableSources/TicketIndexTableViewCell.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Foundation;
+using UIKit;
+
+namespace ResidentAppCross.iOS
+{
+	public partial class TicketIndexTableViewCell : UITableViewCell
+	{
+		public static readonly NSString Key = new NSString ("TicketIndexTableViewCell");
+		public static readonly UINib Nib;
+
+		static TicketIndexTableViewCell ()
+		{
+			Nib = UINib.FromName ("TicketIndexTableViewCell", NSBundle.MainBundle);
+		}
+
+		public TicketIndexTableViewCell (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
---
+++ ResidentAppCross/ResidentAppCross.iOS/Views/TableSources/TicketIndexTableViewCell.designer.cs
@@ -0,0 +1,66 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace ResidentAppCross.iOS
+{
+	[Register ("TicketIndexTableViewCell")]
+	partial class TicketIndexTableViewCell
+	{
+		[Outlet]
+		UIKit.UILabel _commentsLabel { get; set; }
+
+		[Outlet]
+		UIKit.UILabel _dateLabel { get; set; }
+
+		[Outlet]
+		UIKit.UIImageView _headerIcon { get; set; }
+
+		[Outlet]
+		UIKit.UILabel _headerLabel { get; set; }
+
+		[Outlet]
+		UIKit.UILabel _noPhotosLabel { get; set; }
+
+		[Outlet]
+		UIKit.UICollectionView _photoContainer { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (_headerIcon != null) {
+				_headerIcon.Dispose ();
+				_headerIcon = null;
+			}
+
+			if (_commentsLabel != null) {
+				_commentsLabel.Dispose ();
+				_commentsLabel = null;
+			}
+
+			if (_headerLabel != null) {
+				_headerLabel.Dispose ();
+				_headerLabel = null;
+			}
+
+			if (_noPhotosLabel != null) {
+				_noPhotosLabel.Dispose ();
+				_noPhotosLabel = null;
+			}
+
+			if (_photoContainer != null) {
+				_photoContainer.Dispose ();
+				_photoContainer = null;
+			}
+
+			if (_dateLabel != null) {
+				_dateLabel.Dispose ();
+				_dateLabel = null;
+			}
+		}
+	}
+}
