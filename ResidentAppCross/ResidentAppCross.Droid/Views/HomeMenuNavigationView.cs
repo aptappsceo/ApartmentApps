1ea8f91c9f2240fa6ca82d4c140f0a8f5ce5d79d
nitreo <sinitreo@gmail.com>
2016-04-26 03:38:04 +0500

    no message

---
+++ ResidentAppCross/ResidentAppCross.Droid/Views/HomeMenuNavigationView.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Android.Content;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V4.Widget;
+using Android.Support.V7.Widget;
+using Android.Util;
+using Android.Views;
+using DE.Hdodenhof.Circleimageview;
+using ImageViews.Rounded;
+using MvvmCross.Platform;
+using MvvmCross.Plugins.Messenger;
+using ResidentAppCross.Droid.Views.AwesomeSiniExtensions;
+using ResidentAppCross.Droid.Views.Sections;
+
+namespace ResidentAppCross.Droid.Views.Components.Navigation
+{
+    public class HomeMenuNavigationView : NavigationView
+    {
+        private HomeMenuViewModel _viewModel;
+
+        public HomeMenuNavigationView(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+
+        public HomeMenuNavigationView(Context context, IAttributeSet attrs, int defStyleAttr) : base(context, attrs, defStyleAttr)
+        {
+        }
+
+        public HomeMenuNavigationView(Context context, IAttributeSet attrs) : base(context, attrs)
+        {
+        }
+
+        public HomeMenuNavigationView(Context context) : base(context)
+        {
+        }
+
+
+        [Outlet] public AppCompatTextView EmailLabel { get; set; }
+        [Outlet] public AppCompatTextView UsernameLabel { get; set; }
+        [Outlet] public CircleImageView AvatarView { get; set; }
+
+        public HomeMenuViewModel ViewModel
+        {
+            get { return _viewModel; }
+            set
+            {
+                _viewModel = value;
+                if (value != null) OnViewModelSet();
+            }
+        }
+
+        private IMvxMessenger _eventAggregator;
+        private Dictionary<int, ICommand> _commandsMap;
+
+        public IMvxMessenger EventAggregator
+        {
+            get { return _eventAggregator ?? (_eventAggregator = Mvx.Resolve<IMvxMessenger>()); }
+            set { _eventAggregator = value; }
+        }
+
+        public event Action<IMenu> OnRequestContent;
+
+        public virtual void OnViewModelSet()
+        {
+
+            EventAggregator.SubscribeOnMainThread<HomeMenuUpdatedEvent>(evt =>
+            {
+                if (evt.Sender == ViewModel)
+                {
+                    UpdateContent();
+                }
+            });
+            NavigationItemSelected += HomeMenuView_NavigationItemSelected;
+            //ViewModel.UpdateMenuItems();
+
+        }
+
+        public event Action<object, NavigationItemSelectedEventArgs> BeforeNavigate;
+
+        private void HomeMenuView_NavigationItemSelected(object sender, NavigationItemSelectedEventArgs e)
+        {
+            BeforeNavigate?.Invoke(sender,  e);
+            CommandsMap[e.MenuItem.ItemId].Execute(null);
+        }
+
+        public void Refresh()
+        {
+            ViewModel.UpdateMenuItems();
+        }
+
+        public Dictionary<int, ICommand> CommandsMap
+        {
+            get { return _commandsMap ?? (_commandsMap = new Dictionary<int, ICommand>()); }
+            set { _commandsMap = value; }
+        }
+
+
+        private void UpdateContent()
+        {
+            Menu.Clear();
+
+            if (UsernameLabel == null)
+            {
+                var header = this.FindViewById(Resource.Id.NavigationHeader);
+                header.LocateOutlets(this);
+            }
+
+            UsernameLabel.Text = ViewModel?.Username;
+            EmailLabel.Text = ViewModel?.Email;
+
+            if (!string.IsNullOrEmpty(ViewModel?.ProfileImageUrl))
+                ImageExtensions.GetBitmapWithPicasso(ViewModel.ProfileImageUrl).NoFade().Placeholder(Resource.Drawable.avatar_placeholder).CenterCrop().Fit().Into(AvatarView);
+
+            var homeMenu = Menu.AddSubMenu("Home Menu");
+            for (int index = 0; index < ViewModel.MenuItems.Count; index++)
+            {
+                var item = ViewModel.MenuItems[index];
+                var menuitem = homeMenu.Add(0,index,index,item.Name);
+                CommandsMap[menuitem.ItemId] = item.Command;
+                menuitem.SetCheckable(true);
+            }
+
+            OnOnRequestContent(Menu);
+        }
+
+        protected virtual void OnOnRequestContent(IMenu obj)
+        {
+            OnRequestContent?.Invoke(obj);
+        }
+
+    }
+
+}
---
+++ ResidentAppCross/ResidentAppCross.Droid/Views/NavigationDrawerFragment.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V4.View;
+using Android.Support.V4.Widget;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Views;
+using MvvmCross.Droid.Support.V7.AppCompat;
+
+namespace ResidentAppCross.Droid.Views.Components.NavigationDrawer
+{
+//    public class NavigationDrawerFragment : MvxFragment<HomeMenuViewModel>
+//    {
+//        public NavigationDrawerFragment()
+//        {
+//        }
+//
+//        public ObservableCollection<HomeMenuItemViewModel> Data => ViewModel?.MenuItems;
+//
+//        public override void OnViewModelSet()
+//        {
+//            base.OnViewModelSet();
+//            Recycler.SetAdapter(new NavigationDrawer.NavigationDrawerAdapter(Activity, Data));
+//
+//        }
+//
+//        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+//        {
+//            //return base.OnCreateView(inflater, container, savedInstanceState);
+//
+//            var layout = inflater.Inflate(Resource.Layout.nav_main,container,false);
+//            Recycler = layout.FindViewById<RecyclerView>(Resource.Id.drawerList);
+//            Recycler.SetLayoutManager(new LinearLayoutManager(Activity));
+//            return layout;
+//
+//        }
+//
+//        public RecyclerView Recycler { get; set; }
+//
+//        public void Setup(int fragmentId, DrawerLayout layout, Toolbar toolbar)
+//        {
+//            Container = Activity.FindViewById(fragmentId);
+//            DrawerLayout = layout;
+//            DrawerToggle = new UsefulActionBarDrawerToggle(Activity,layout,toolbar,Resource.String.drawer_open,Resource.String.drawer_close);
+//            DrawerLayout.AddDrawerListener(DrawerToggle);
+//            DrawerLayout.Post(() => DrawerToggle.SyncState());
+//        }
+//
+//        public UsefulActionBarDrawerToggle DrawerToggle { get; set; }
+//
+//        public View Container { get; set; }
+//        public DrawerLayout DrawerLayout { get; set; }
+//    }
+
+    public class UsefulActionBarDrawerToggle : ActionBarDrawerToggle
+    {
+        protected UsefulActionBarDrawerToggle(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+
+        public UsefulActionBarDrawerToggle(Activity activity, DrawerLayout drawerLayout, Toolbar toolbar, int openDrawerContentDescRes, int closeDrawerContentDescRes) : base(activity, drawerLayout, toolbar, openDrawerContentDescRes, closeDrawerContentDescRes)
+        {
+            this.Activity = activity;
+            this.Layout = drawerLayout;
+            this.Toolbar = toolbar;
+        }
+
+        public UsefulActionBarDrawerToggle(Activity activity, DrawerLayout drawerLayout, int openDrawerContentDescRes, int closeDrawerContentDescRes) : base(activity, drawerLayout, openDrawerContentDescRes, closeDrawerContentDescRes)
+        {
+            this.Activity = activity;
+            this.Layout = drawerLayout;
+        }
+
+        public Toolbar Toolbar { get; set; }
+        public DrawerLayout Layout { get; set; }
+        public Activity Activity { get; set; }
+
+        public Action<View> Opened { get; set; }
+        public Action<View> Closed { get; set; }
+        public Action Opening { get; set; }
+
+        public override void OnDrawerOpened(View drawerView)
+        {
+            base.OnDrawerOpened(drawerView);
+            Opened?.Invoke(drawerView);
+        }
+
+        public override void OnDrawerClosed(View drawerView)
+        {
+            base.OnDrawerClosed(drawerView);
+            Closed?.Invoke(drawerView);
+
+        }
+
+        public override void OnDrawerStateChanged(int newState)
+        {
+            if (newState == DrawerLayout.StateSettling)
+            {
+                if (!Layout.IsDrawerOpen(GravityCompat.Start))
+                {
+                    Opening?.Invoke();
+                }
+            }
+        }
+
+        public Action<int> OnDrawerOpening { get; set; }
+
+        public override void OnDrawerSlide(View drawerView, float slideOffset)
+        {
+            base.OnDrawerSlide(drawerView, slideOffset);
+            Toolbar.Alpha = (1 - slideOffset/2);
+        }
+    }
+}
