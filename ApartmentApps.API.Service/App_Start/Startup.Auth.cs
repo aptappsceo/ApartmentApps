ba4b3c5b0965dbbac424a0d996666e33078233ce
Micah Osborne <micahosborne@gmail.com>
2016-04-26 17:02:02 -0400

    no message

--- ApartmentApps.API.Service/App_Start/Startup.Auth.cs
+++ ApartmentApps.API.Service/App_Start/Startup.Auth.cs
@@ -39,7 +39,7 @@ namespace ApartmentApps.API.Service
                 TokenEndpointPath = new PathString("/Token"),
                 Provider = new ApplicationOAuthProvider(PublicClientId),
                 AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
-                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(800),
                 // In production mode set AllowInsecureHttp = false
                 AllowInsecureHttp = true
             };
