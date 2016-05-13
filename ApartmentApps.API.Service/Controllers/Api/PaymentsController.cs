80879c14ef5970af53f0b9f840e0f8831771b4f3
Micah Osborne <micahosborne@gmail.com>
2016-05-13 10:53:11 -0400

    no message

--- ApartmentApps.API.Service/Controllers/Api/PaymentsController.cs
+++ ApartmentApps.API.Service/Controllers/Api/PaymentsController.cs
@@ -5,36 +5,7 @@ using ApartmentApps.Data.Repository;
 
 namespace ApartmentApps.API.Service.Controllers
 {
-    public interface IPaymentsService
-    {
-        Task<MakePaymentResult> MakePayment(MakePaymentBindingModel makePaymentBindingModel);
-    }
-
-    public class FortePaymentsService : IPaymentsService
-    {
-        public FortePaymentsService(IUserContext context)
-        {
-            MerchantId = context.CurrentUser.Property.Name;
-        }
-
-        public string MerchantId { get; set; }
-
-        public async Task<MakePaymentResult> MakePayment(MakePaymentBindingModel makePaymentBindingModel)
-        {
-            //var merchantId = "1234";
-            //var apiClientId = "2000";
-            //var key = "";
-            //var client = new ApartmentApps.Payments.Forte.Forte.Client.ClientServiceClient();
-            //var auth = Authenticate.GetClientAuthTicket(apiClientId, key);
-            //var result = await client.createClientAsync(auth, new ClientRecord()
-            //{
-            //     FirstName = "",
-            //     LastName = "",
-            //});
-            return new MakePaymentResult();
-        }
-    }
-
+ 
 
     [Authorize]
     [RoutePrefix("api/Payments")]
@@ -55,12 +26,5 @@ namespace ApartmentApps.API.Service.Controllers
         }
     }
 
-    public class MakePaymentResult
-    {
-        
-    }
-    public class MakePaymentBindingModel
-    {
-        
-    }
+   
 }
