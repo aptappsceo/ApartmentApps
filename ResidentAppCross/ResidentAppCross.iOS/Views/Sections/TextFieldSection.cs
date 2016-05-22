7d9ad70b751e36447d0045e7d77da4db5667558e
sinitreo - mac <sinitreo@gmail.com>
2016-05-22 00:17:53 -0700

    no message

---
+++ ResidentAppCross/ResidentAppCross.iOS/Views/Sections/TextFieldSection.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace ResidentAppCross.iOS
+{
+	partial class TextFieldSection : UIView
+	{
+		public TextFieldSection (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
