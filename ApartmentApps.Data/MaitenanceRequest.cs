a53128f0e04d9982c3489d4d6c9bdef528bbd13f
Micah Osborne <micahosborne@gmail.com>
2016-10-04 17:53:13 -0400

    no message

--- ApartmentApps.Data/MaitenanceRequest.cs
+++ ApartmentApps.Data/MaitenanceRequest.cs
@@ -4,7 +4,6 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity.ModelConfiguration;
 using System.Linq;
-using ApartmentApps.Api.ViewModels;
 using Korzh.EasyQuery;
 
 namespace ApartmentApps.Data
