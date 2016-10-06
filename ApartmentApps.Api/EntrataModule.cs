db37bdd4e33e64aec2bfa9cea87e47562dd058bf
Micah Osborne <micahosborne@gmail.com>
2016-10-06 12:29:01 -0400

    no message

--- ApartmentApps.Api/EntrataModule.cs
+++ ApartmentApps.Api/EntrataModule.cs
@@ -10,8 +10,11 @@ namespace ApartmentApps.Api
 {
     public class EntrataModule : PropertyIntegrationModule<EntrataConfig>
     {
-        public EntrataModule(ApplicationDbContext dbContext,PropertyContext context, DefaultUserManager manager,  IRepository<EntrataConfig> configRepo, IUserContext userContext, IKernel kernel) : base(dbContext, context, manager, configRepo, userContext, kernel)
+        private readonly IRepository<ApplicationUser> _userRepo;
+
+        public EntrataModule(ApplicationDbContext dbContext, PropertyContext context, DefaultUserManager manager, IRepository<ApplicationUser> userRepo, IRepository<EntrataConfig> configRepo, IUserContext userContext, IKernel kernel) : base(dbContext, context, manager, configRepo, userContext, kernel)
         {
+            _userRepo = userRepo;
         }
 
         public override void Execute(ILogger logger)
@@ -34,50 +37,72 @@ namespace ApartmentApps.Api
                     if (result?.response?.result?.PhysicalProperty != null)
                         foreach (var property in result?.response?.result?.PhysicalProperty?.Property)
                         {
-                            foreach (var ilsUnit in property.ILS_Unit.Select(p=>p.Units.Unit))
+                            foreach (var ilsUnit in property.ILS_Unit.Select(p => p.Units.Unit))
                             {
-                                ImportUnit(logger, ilsUnit.BuildingName,ilsUnit.MarketingName);
+                                ImportUnit(logger, ilsUnit.BuildingName, ilsUnit.MarketingName);
                             }
                         }
 
                 var customers =
                     entrataClient.GetCustomers(item.EntrataPropertyId).Result.Response.Result.Customers.Customer;
-                var mitsLeasesResult = entrataClient.GetMitsLeases(item.EntrataPropertyId).Result;
-                var leases = mitsLeasesResult.response.result.LeaseApplication.LA_Lease;
+                var customersOld =
+                    entrataClient.GetCustomers(item.EntrataPropertyId, "6").Result.Response.Result.Customers.Customer;
+
+                // Archive all the old customers
+                foreach (var oldCustomer in customersOld)
+                {
+                    if (!string.IsNullOrEmpty(oldCustomer.Email?.Trim()))
+                    {
+                        var customerEmail = oldCustomer.Email;
+                        var user = DbContext.Users.FirstOrDefault(p => p.Email == customerEmail && p.Archived == false);
+                        if (user != null)
+                        {
+                            user.Archived = true;
+                            DbContext.SaveChanges();
+                            logger.Info($"Email {oldCustomer.Email} Archived");
+                        }
+
+                    }
+                }
+
+
+                //var mitsLeasesResult = entrataClient.GetMitsLeases(item.EntrataPropertyId).Result;
+                //var leases = mitsLeasesResult.response.result.LeaseApplication.LA_Lease;
 
                 foreach (var customer in customers)
                 {
 
                     Building building;
                     Unit unit;
-                    ImportUnit(logger,customer.BuildingName,customer.UnitNumber,out unit, out building);
+                    ImportUnit(logger, customer.BuildingName, customer.UnitNumber, out unit, out building);
 
-                    var user = ImportCustomer(this,unit.Id, customer.PhoneNumber.NumbersOnly(),customer.City, customer.Email, customer.FirstName, customer.LastName,customer.MiddleName,customer.Gender,customer.PostalCode,customer.State,customer.Address);
+                    var user = ImportCustomer(this, unit.Id, customer.PhoneNumber.NumbersOnly(), customer.City, customer.Email, customer.FirstName, customer.LastName, customer.MiddleName, customer.Gender, customer.PostalCode, customer.State, customer.Address);
                     if (user == null) continue;
-
-                    var leaseId = customer.LeaseId?.Identification[0]?.IDValue.ToString();
                     var entrataId = customer.Attributes?.Id;
                     user.SyncId = entrataId;
                     _context.SaveChanges();
 
-                    if (leaseId == null || entrataId == null) continue;
 
-                    var lease = leases.FirstOrDefault(p => p.Identification.IDValue == leaseId);
-                    if (lease == null) continue;
+                    //var leaseId = customer.LeaseId?.Identification[0]?.IDValue.ToString();
 
-                    var moveOutInfo =
-                        lease.LeaseEvents.LeaseEvent.FirstOrDefault(p => p.Attributes.EventType == "ActualMoveOut");
-                    if (moveOutInfo != null)
-                    {
-                        user.Archived = true;
-                        _context.SaveChanges();
-                        logger.Info($"Archiving User {customer.FirstName} {customer.LastName} {leaseId} {entrataId} ");
-                    }
+                    //if (leaseId == null || entrataId == null) continue;
+
+                    //var lease = leases.FirstOrDefault(p => p.Identification.IDValue == leaseId);
+                    //if (lease == null) continue;
+
+                    //var moveOutInfo =
+                    //    lease.LeaseEvents.LeaseEvent.FirstOrDefault(p => p.Attributes.EventType == "ActualMoveOut");
+                    //if (moveOutInfo != null)
+                    //{
+                    //    user.Archived = true;
+                    //    _context.SaveChanges();
+                    //    logger.Info($"Archiving User {customer.FirstName} {customer.LastName} {leaseId} {entrataId} ");
+                    //}
                 }
             }
 
         }
 
-        
+
     }
 }
--- ApartmentApps.Tests/DbQueryTest.cs
+++ ApartmentApps.Tests/DbQueryTest.cs
@@ -1,7 +1,12 @@
 using System;
+using System.Data.SqlClient;
+using System.IO;
 using ApartmentApps.Api.ViewModels;
 using ApartmentApps.Data;
 using ApartmentApps.Portal.Controllers;
+using DBDiff.Schema.SQLServer.Generates.Generates;
+using DBDiff.Schema.SQLServer.Generates.Model;
+using DBDiff.Schema.SQLServer.Generates.Options;
 using Korzh.EasyQuery;
 using Korzh.EasyQuery.Db;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -9,6 +14,86 @@ using Ninject;
 
 namespace ApartmentApps.Tests
 {
+    [TestClass]
+    public class DbTasks
+    {
+        public static string ConnectionString2 = "Server=aptapps.database.windows.net,1433;Database=ApartmentApps_Development;User ID=aptapps;Password=Asdf1234!@#$;";
+        public static string ConnectionString1 = "Server=aptapps.database.windows.net,1433;Database=ApartmentApps;User ID=aptapps;Password=Asdf1234!@#$;";
+        public static SqlOption SqlFilter = new SqlOption();
+        static void SaveFile(string filenmame, string sql)
+        {
+            if (!String.IsNullOrEmpty(filenmame))
+            {
+                StreamWriter writer = new StreamWriter(filenmame, false);
+                writer.Write(sql);
+                writer.Close();
+            }
+        }
+        static Boolean TestConnection(string connectionString1, string connectionString2)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection();
+                connection.ConnectionString = connectionString1;
+                connection.Open();
+                connection.Close();
+                connection.ConnectionString = connectionString2;
+                connection.Open();
+                connection.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void BackupProdDb()
+        {
+            
+        }
+
+        [TestMethod]
+        public void GenerateScripts()
+        {
+            try
+            {
+                Database origin;
+                Database destination;
+                if (TestConnection(ConnectionString1, ConnectionString2))
+                {
+                    Generate sql = new Generate();
+                    sql.ConnectionString = ConnectionString1;
+                    Console.WriteLine("Reading first database...");
+                    sql.Options = SqlFilter;
+                    origin = sql.Process();
+                    sql.ConnectionString = ConnectionString2;
+                    Console.WriteLine("Reading second database...");
+                    destination = sql.Process();
+                    Console.WriteLine("Comparing databases schemas...");
+                    origin = Generate.Compare(origin, destination);
+                    //if (!arguments.OutputAll)
+                    //{
+                    //    // temporary work-around: run twice just like GUI
+                    //    origin.ToSqlDiff();
+                    //}
+                    origin.ToSqlDiff();
+                    Console.WriteLine("Generating SQL file...");
+                    Console.WriteLine();
+
+                    SaveFile("UpgradeDb.sql", origin.ToSqlDiff().ToSQL());
+
+
+                    //completedSuccessfully = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(String.Format("{0}\r\n{1}\r\n\r\nPlease report this issue at http://opendbiff.codeplex.com/workitem/list/basic\r\n\r\n", ex.Message, ex.StackTrace));
+            }
+        }
+    }
+
     [TestClass]
     public class DbQueryTest : PropertyTest
     {
