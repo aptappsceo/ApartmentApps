3e9a0b1ea6c2f87cb24458fa299349bdece0b86c
Micah Osborne <micahosborne@gmail.com>
2016-10-12 11:40:52 -0400

    no message

--- ApartmentApps.Api/Services/BaseViewModel.cs
+++ ApartmentApps.Api/Services/BaseViewModel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using ApartmentApps.Forms;
@@ -10,5 +11,32 @@ namespace ApartmentApps.Portal.Controllers
         public string Id { get; set; }
 
         public List<ActionLinkModel> ActionLinks { get; set; } = new List<ActionLinkModel>();
+    }
+
+    public class PageViewModel
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public List<ActionLinkModel> ActionLinks { get; set; } = new List<ActionLinkModel>();
+        public Type ViewModelType => ViewModelObject.GetType();
+        public object ViewModelObject { get; set; }
+
+        public string View { get; set; }
+    }
+
+    public class PageSectionViewModel
+    {
+
+        /// <summary>
+        /// Similar to col-md-12 -- 12 being an entire row
+        /// </summary>
+        public int ColumnSpan { get; set; }
+
+        public Type ViewModelType => ViewModel.GetType();
+        public object ViewModel { get; set; }
+
+        public string View { get; set; }
     }
 }
