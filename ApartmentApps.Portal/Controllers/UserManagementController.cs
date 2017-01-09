c94fb0034f37b92f45bae140212c340a4c46800a
Micah Osborne <micahosborne@gmail.com>
2017-01-09 00:16:50 -0500

    no message

--- ApartmentApps.Portal/Controllers/UserManagementController.cs
+++ ApartmentApps.Portal/Controllers/UserManagementController.cs
@@ -156,7 +156,7 @@ namespace ApartmentApps.Portal.Controllers
             {
                 FromEmail = "auto@apartmentapps.com",
                 ToEmail = user.Email,
-                Message = $"Username: {user.UserName} <br/>Password: {user.Email}<br/><b>It is highly recommended that you change your password once you log-in.</b>",
+                Message = $"Username: {user.UserName} Password: {newPassword}<br/><b>It is highly recommended that you change your password once you log-in.</b>",
                 User = userMapper.ToViewModel(user),
                 Subject = "Here is your account information.",
                 Links = new Dictionary<string, string>()
