6f9f5af6359628d5ee4b3412f9cb6124c8814592
Micah Osborne <micahosborne@gmail.com>
2017-01-15 21:32:11 -0500

    no message

--- ApartmentApps.Tests/MaitenanceRequestsControllerTests.cs
+++ ApartmentApps.Tests/MaitenanceRequestsControllerTests.cs
@@ -101,13 +101,13 @@ namespace ApartmentApps.Tests
             // Submit another request to test the list
             SubmitMaintenanceRequest();
 
-            var config = Context.Kernel.Get<MaintenanceModule>().Config;
+            var config = Context.Kernel.Get<ConfigProvider<MaintenanceConfig>>().Config;
             config.SupervisorMode = true;
             var controllerRequestList = ApiController.ListRequests();
             Assert.AreEqual(controllerRequestList.Count(),1);
             config.SupervisorMode = false;
             controllerRequestList = ApiController.ListRequests();
-            Assert.AreEqual(controllerRequestList.Count(), 2);
+            Assert.AreEqual(controllerRequestList.Count(),1);
         }
 
         private void SubmitMaintenanceRequest()
