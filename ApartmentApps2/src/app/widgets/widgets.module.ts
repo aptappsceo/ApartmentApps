a4557d069eb2ef86798ea64edb1e24de91563d3a
Micah Osborne <micahosborne@gmail.com>
2017-05-26 14:28:58 -0400

    stuff

--- ApartmentApps2/src/app/widgets/widgets.module.ts
+++ ApartmentApps2/src/app/widgets/widgets.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeedItemComponent } from './feeditem/feeditem.component';
 import { TitleComponent } from './title/title.component';
+import { CommentsComponent } from './comments/comments.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [FeedItemComponent, TitleComponent],
+  declarations: [FeedItemComponent, TitleComponent, CommentsComponent],
   exports: [FeedItemComponent, TitleComponent]
 })
 export class WidgetsModule { }
