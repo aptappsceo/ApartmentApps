2d6795fef4f7a3def72ea308481d6037faab2587
Micah Osborne <micahosborne@gmail.com>
2017-06-14 15:09:27 -0400

    Stuff

--- ApartmentApps2/src/app/maintenance/maintenance.module.ts
+++ ApartmentApps2/src/app/maintenance/maintenance.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateWorkOrderComponent } from './create-work-order/create-work-order.component';
+import { MaintenancePageComponent } from './maintenance-page/maintenance-page.component';
+import { MaintenanceFormComponent } from './maintenance-form/maintenance-form.component';
 
 @NgModule({
   imports: [
@@ -9,6 +11,6 @@ import { CreateWorkOrderComponent } from './create-work-order/create-work-order.
   exports: [
     CreateWorkOrderComponent
   ],
-  declarations: [CreateWorkOrderComponent]
+  declarations: [CreateWorkOrderComponent, MaintenancePageComponent, MaintenanceFormComponent]
 })
 export class MaintenanceModule { }
---
+++ ApartmentApps2/src/app/messaging/messaging.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MessagingPageComponent } from './messaging-page/messaging-page.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [MessagingPageComponent]
+})
+export class MessagingModule { }
